6fca4e3e79461e49ea7754ac070daa524c3282d6
Alain Guillon <zyrass@outlook.fr>
2026-05-16 16:56:01 +0200

    fix: remove scratch directory from repository and add .gitignore

--- scratch/fix_encoding.ps1
+++
@@ -1,5 +0,0 @@
-$files = Get-ChildItem -Path "docs", "README.md" -Recurse -Include "*.md"
-foreach ($file in $files) {
-    $content = [System.IO.File]::ReadAllText($file.FullName)
-    [System.IO.File]::WriteAllText($file.FullName, $content, (New-Object System.Text.UTF8Encoding($false)))
-}
